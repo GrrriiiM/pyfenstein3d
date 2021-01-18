91f7dc53494b7839dee29a9db26a0b2c64dd1ff1
Allison Ferreira de Oliveira <allison.f.oliveira@hotmail.com>
2021-01-18 00:34:33 -0300

    corrigindo teste unitario

--- tests/test_weapon.py
+++ tests/test_weapon.py
@@ -1,13 +1,15 @@
 from engine import Weapon
+from engine import Player
 
 def test_shoot():
     weapon = Weapon(10, 0.5)
     grid = None
+    person = Player(0, 0, 0, None, "")
     assert weapon.shoot_interval == 0.5;
     assert weapon.is_shooting == False
     assert weapon.shoot_animation.is_animating == False
     assert weapon.shoot_animation.factor == 0
-    weapon.update(0.1, grid)
+    weapon.update(0.1, person, grid)
     assert weapon.is_shooting == False
     assert weapon.shoot_animation.is_animating == False
     assert weapon.shoot_animation.factor == 0
@@ -15,28 +17,28 @@ def test_shoot():
     assert weapon.is_shooting == True
     assert weapon.shoot_animation.is_animating == False
     assert weapon.shoot_animation.factor == 0
-    weapon.update(0.1, grid)
+    weapon.update(0.1, person, grid)
     assert weapon.is_shooting == True
     assert weapon.shoot_animation.is_animating == True
     assert weapon.shoot_animation.factor == 0.2
-    weapon.update(0.1, grid)
+    weapon.update(0.1, person, grid)
     assert weapon.is_shooting == True
     assert weapon.shoot_animation.is_animating == True
     assert weapon.shoot_animation.factor == 0.4
-    weapon.update(0.3, grid)
+    weapon.update(0.3, person, grid)
     assert weapon.is_shooting == True
     assert weapon.shoot_animation.is_animating == False
     assert weapon.shoot_animation.factor == 1
-    weapon.update(0.3, grid)
+    weapon.update(0.3, person, grid)
     assert weapon.is_shooting == True
     assert weapon.shoot_animation.is_animating == True
     assert weapon.shoot_animation.factor == 0.6
     weapon.is_shooting = False
-    weapon.update(0.3, grid)
+    weapon.update(0.3, person, grid)
     assert weapon.is_shooting == False
     assert weapon.shoot_animation.is_animating == False
     assert weapon.shoot_animation.factor == 1
-    weapon.update(0.3, grid)
+    weapon.update(0.3, person, grid)
     assert weapon.is_shooting == False
     assert weapon.shoot_animation.is_animating == False
     assert weapon.shoot_animation.factor == 0
