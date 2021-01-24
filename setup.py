d599beafb2c07d58cd632c7113a378d2ef10ec13
Allison Ferreira de Oliveira <allison.f.oliveira@hotmail.com>
2021-01-24 02:03:02 -0300

    alterando versao

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
     install_requires = f.read().splitlines()
 
 setuptools.setup(name='pyfenstein3d',
-                 version='1.3.1',
+                 version='1.3.2',
                  description='Projeto realizado em python com intuido educacional de tentar reproduzir o jogo Wolfenstein 3d no prompt de comando.',
                  long_description=readme,
                  long_description_content_type='text/markdown',
